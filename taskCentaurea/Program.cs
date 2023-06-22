f4a785179a6322cc8d5d16e8f631498d0870aa2b
ZoldikDS <zoldikds@mail.ru>
2023-06-23 00:19:48 +0300

    Added task

---
+++ taskCentaurea/Program.cs
@@ -0,0 +1,38 @@
+﻿string[] exampleWords = 
+{
+    "Эй",
+    "Привет",
+    "это",
+    "тестовый",
+    "набор",
+    "слов",
+    "Абрикос",
+};
+
+string vowels = "аиеёоуыэюя";
+
+string StringBuilder(string[] words) {
+    
+    int[] vowelsCount = new int[words.Length];
+
+    int index = 0;
+
+    foreach (var word in words)
+    {
+        foreach (var letter in word.ToLower())
+        {
+            if (vowels.IndexOf(letter) != -1)
+                vowelsCount[index]++;
+        }
+        
+        index++;
+    }
+
+    Array.Sort(vowelsCount, words);
+    
+    string result = String.Join(" ", words);
+
+    return result;
+}
+
+Console.WriteLine(StringBuilder(exampleWords));
